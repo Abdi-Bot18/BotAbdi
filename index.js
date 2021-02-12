82998581dc62a99ed37c86cb9b604abbd0771cbf
Abdi-Bot18 <78109744+Abdi-Bot18@users.noreply.github.com>
2021-02-12 11:27:39 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let { spawn } = require('child_process')
 let path = require('path')
 let fs = require('fs')
 const CFonts  = require('cfonts')
-CFonts.say('BOT ABDI', {
+CFonts.say('Abdi-Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
