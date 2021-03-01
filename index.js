e419b7e923fbdc0f28e85cc7a88f2d5865536dca
Abdi-Bot18 <78109744+Abdi-Bot18@users.noreply.github.com>
2021-03-02 02:09:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\nWhatsApp Bot', {
+CFonts.say('Abdi-Bot\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
+CFonts.say(`'${package.name}' By Abdi-Bot`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
